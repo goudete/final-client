1b51bbeacc0663bdfe8f32ea9e6a857891098079
goudete <goudetenrique@gmail.com>
2020-11-21 13:27:53 -0800

    get specific class integrated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Navbaroo from '../../components/Navbar/Navbar';
 import axios from 'axios';
-import { Card, Elevation } from "@blueprintjs/core";
+import { Card, Drawer, Elevation } from "@blueprintjs/core";
 import { Subscribe } from 'unstated';
 import AuthContainer from '../../containers/AuthContainer'
 import {
@@ -15,9 +15,12 @@ class Home extends React.Component {
     super(props)
     this.state = {
       classes: [],
-      profs: []
+      profs: [],
+      classDetails: null,
+      toggleDrawer: false
     }
-    
+    this.clickCard = this.clickCard.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
 
   componentDidMount() {
@@ -37,6 +40,23 @@ class Home extends React.Component {
       }
     })
   }
+  clickCard(id) {
+    axios.get(`/class/${id}`)
+    .then(res => {
+      const classDetails = res.data;
+      if (classDetails) {
+        this.setState({ 
+          classDetails: classDetails.course[0],
+          toggleDrawer: true
+        });
+      }
+    })
+  }
+  toggle() {
+    this.setState({
+      toggleDrawer: null
+    })
+  }
 
   render() {
     const loggedIn = this.props.auth.state.loggedIn
@@ -49,14 +69,47 @@ class Home extends React.Component {
             {this.state.classes.map((c) => (
               <div className="flexItem">
 
-                <Card className="classesCard" interactive={true} elevation={Elevation.TWO}>
+                <Card className="classesCard" interactive={true} elevation={Elevation.TWO} onClick={() => this.clickCard(c.id)}>
                   <h3>{c.name}</h3>
                   <p>{ c.description.length > 350 ? (c.description.substring(0, 350) + '...') : (c.description) }</p>
+                  <div className="infoContainer">
+                    <div className="flexItem">
+                      <h5>{c.classroom}</h5>
+                    </div>
+                    <div className="flexItem">
+                    <h5>{c.days}</h5>
+                    </div>
+                    <div className="flexItem">
+                      <h5>{c.start}-{c.over}</h5>
+                    </div>
+                  </div>
                 </Card>
-
               </div>
             ))}
           </div>
+          <Drawer
+            isOpen={this.state.toggleDrawer}
+            onClose={this.toggle}
+          >
+            <div className="drawerContainer">
+              <h1>{this.state.classDetails && this.state.classDetails.name}</h1>
+              <h4>{this.state.classDetails && this.state.classDetails.description}</h4>
+              <div className="infoContainer">
+                <div className="flexItem">
+                  <h3>{this.state.classDetails && this.state.classDetails.classroom}</h3>
+                </div>
+                <div className="flexItem">
+                  <h3>{this.state.classDetails && this.state.classDetails.days}</h3>
+                </div>
+                <div className="flexItem">
+                  <h3>{this.state.classDetails && this.state.classDetails.start } - {this.state.classDetails && this.state.classDetails.over }</h3>
+                  
+                </div>
+              </div>
+            </div>
+          
+          </Drawer>
+
       </Fragment>
       ) :
       (
